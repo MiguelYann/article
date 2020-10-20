d84ed842cf088d33268515f118be5abf3b0c1735
MiguelYann <miguel.tsamba@kaibee.fr>
2020-10-20 12:07:45 +0200

    🚗

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('article-sandbox app is running!');
+    expect(compiled.querySelector('.content').textContent).toContain('Hey!');
   });
 });
