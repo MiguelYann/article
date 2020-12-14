33cdf3a4e0b22c971da16428dbb78b9849dcf097
MiguelYann <miguel.tsamba@kaibee.fr>
2020-12-14 22:36:05 +0100

    🚗🚗

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'article-sandbox';
+
+  list = ['Java', 'JS', 'Dart', 'Angular', 'Spring', 'Flutter'];
+
+  test = () => console.log('Hello');
 }
+
+// bdbda5c75102b76e2ba4
+
+// 6920e79097fea0b8bae4
